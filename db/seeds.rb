4d57f8b517fd441eeae24a68a8411541580877ef
“Andrew <andrewwoods88@gmail.com>
2018-05-07 11:49:00 -0400

    commented out seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-@user = User.create(email: "test@test.com", password_digest: "password", username: "andrew")
+# @user = User.create(email: "test@test.com", password_digest: "password", username: "andrew")
 
-puts "1 user created"
+# puts "1 user created"
 
 
-100.times do |article|
-    Article.create!( title: "South Rentail Rink", 
-                    description: "31 Richmond Dr", 
-                    user_id: @user.id)
-end
+# 100.times do |article|
+#     Article.create!( title: "South Rentail Rink", 
+#                     description: "31 Richmond Dr", 
+#                     user_id: @user.id)
+# end
 
-puts "100 articless have been created"
+# puts "100 articless have been created"

d43d3dd37d4280ed505ac77dfaf57df6548dd8ee
“Andrew <andrewwoods88@gmail.com>
2018-05-10 13:04:26 -0400

    built handle change method, and added state for selector

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -6,13 +6,25 @@ class Colorpicker extends React.Component {
   
   constructor(props){
     super(props);
-
+                  
+    this.state = { selector: props.selector }
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (color) => {
     console.log(color.hex);
+
+    // set backgorund style of body to value of color
     document.body.style.background = color.hex;
+
+    // ---------------------------------------------------
+    // THEN - above part and below part not really related
+
+    // select the form input, and pass it an id from state
+    let input_element = document.querySelector('#'+this.state.selector)
+
+    // make the value of the dom element = to the hex color being passed in as a parameter
+    input_element.value = color.hex
   }
   
   render() {
